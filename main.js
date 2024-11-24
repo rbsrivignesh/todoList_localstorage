8f64d063573a4a7e14ca78b3f52f84b5fcc23e68
rbsvcode <rbsvcode@gmail.com>
2024-11-24 13:52:45 +0530

    added edit/save

--- main.js
+++ main.js
@@ -42,24 +42,54 @@ function createTodoItem(todo,todoIndex){
                     <svg fill= "transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg>
                 </label>
                 <label for="${todoID}" class="todo-text">
-                    ${todoText}
+                  
+                    <input id="text" class="todo-text" type="text" value = "${todoText}">
                 </label>
+                <button class ="edit">
+                EDIT
+                </button>
                 <button class="delete-button">
                     <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
                 </button>`
-
+    const inputText = todoLI.querySelector("#text");
+    inputText.setAttribute("readonly","readonly");
     const deleteButton = todoLI.querySelector(".delete-button");
     deleteButton.addEventListener('click',()=>{
         deleteTodoItem(todoIndex);
         saveTodos();
         updateTodoList();
     })
+
     const checkbox = todoLI.querySelector("input");
     checkbox.addEventListener('change',()=>{
         allTodos[todoIndex].completed = checkbox.checked;
         saveTodos()
     })
     checkbox.checked = todo.completed;
+    
+    // todoInput.addEventListener('change',()=>{
+    //     // alert("is working")
+    //     allTodos[todoIndex].text = todoInput.value;
+    //     saveTodos();
+    //     // updateTodoList();
+    // })
+    // updateTodoList();
+
+    const edit = todoLI.querySelector(".edit");
+    edit.addEventListener('click',()=>{
+        if(edit.innerText=="EDIT"){
+            inputText.removeAttribute("readonly");
+            edit.innerText = "SAVE"
+        }
+        else{
+            allTodos[todoIndex].text = inputText.value;
+            saveTodos();
+             updateTodoList();
+            inputText.setAttribute("readonly","readonly");
+             edit.innerText = "EDIT"
+        }
+
+    })
     return todoLI;
 }
 
